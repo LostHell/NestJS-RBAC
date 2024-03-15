563e3e3b1c885608bd203bfc95ee389f97b73a6c
losthell <losthell92@gmail.com>
2024-03-15 22:01:28 +0100

    Added docker-compose.yml & configurations for database in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseSettings } from './config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // Import the ConfigModule
+      inject: [ConfigService], // Inject the ConfigService
+      useFactory: (configService: ConfigService) => databaseSettings(configService), // Use the ConfigService to pass configuration values
+    }),
+  ],
   controllers: [],
   providers: [],
 })
